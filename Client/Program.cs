7b5ac302be02bc91f8b376663d0a4c2993f4a17e
Andrei <Andreiboghean00@gmail.com>
2020-07-16 18:57:00 +0100

    first working version

---
+++ Client/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    static class Program
+    {
+        static Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            LoopSend();
+            Console.ReadLine();
+        }
+
+        static void LoopConnect()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    ClientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"attempt number {attempts} has failed");
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("connected at attempt number " + attempts);
+        }
+
+        static void LoopSend()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("enter a message");
+                ClientSocket.Send(Encoding.ASCII.GetBytes(Console.ReadLine()));
+
+                byte[] RecievedBuffer = new byte[1024];
+
+                int rec = ClientSocket.Receive(RecievedBuffer);
+
+                byte[] Recieved = new byte[rec];
+                Array.Copy(RecievedBuffer, Recieved, rec);
+                /*
+                Console.BackgroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(Encoding.ASCII.GetString(Recieved) );
+                Console.BackgroundColor = ConsoleColor.Black;
+                */
+            }
+        }
+    }
+}
---
+++ Host/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTest
+{
+    static class Program
+    {
+        static byte[] buffer = new byte[1024];
+        static List<Socket> ClientSocketsList = new List<Socket>();
+        static Socket MainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Host";
+            StartServer();
+            Console.ReadLine();
+        }
+
+        static void StartServer()
+        {
+            Console.WriteLine("starting server...");
+            MainSocket.Bind(new IPEndPoint(IPAddress.Any, 100) );
+
+            MainSocket.Listen(5);
+            MainSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = MainSocket.EndAccept(AR);
+            ClientSocketsList.Add(socket);
+            Console.Clear();
+            Console.WriteLine("client connected");
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallback), socket);
+            MainSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        static void RecieveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int recieved = socket.EndReceive(AR);
+            byte[] DataBuffer = new byte[recieved];
+            Array.Copy(buffer, DataBuffer, recieved);
+
+            string text = Encoding.ASCII.GetString(DataBuffer);
+            Console.WriteLine("text recieved: " + text);
+
+            byte[] DataToSend = Encoding.ASCII.GetBytes("message recieved");
+            socket.BeginSend(DataToSend, 0, DataToSend.Length, SocketFlags.None, new AsyncCallback(SendCallBack), socket);
+
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(RecieveCallback), socket);
+        }
+
+        static void SendCallBack(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+}
